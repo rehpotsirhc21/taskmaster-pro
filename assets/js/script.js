2584515bb9b5316d6583bd36c39c7459fac5d498
Christopher Langner <langnerc@icloud.com>
2022-03-29 08:40:43 -0500

    added delete functionality and mobile touch

--- assets/js/script.js
+++ assets/js/script.js
@@ -189,5 +189,20 @@ $("#remove-tasks").on("click", function () {
   saveTasks();
 });
 
+//allow drop into the delete area of the UI
+$("#trash").droppable({
+  accept: ".card .list-group-item", 
+  tolerance: "touch", 
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop")
+  }, 
+  over: function(event, ui) {
+    console.log("over")
+  }, 
+  out: function(event, ui){
+    console.log("out")
+  }
+})
 // load tasks for the first time
 loadTasks();
